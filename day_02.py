059b5bb53dfadf67428327c497a4d341a6c689bc
snadis <snadis@upp.lv>
2020-12-03 13:09:29 +0200

    refaktorings day2

--- day_02.py
+++ day_02.py
@@ -1,4 +1,13 @@
 import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Password:
+    number_a: int
+    number_b: int
+    character: str
+    password: str
 
 
 def read_file(path):
@@ -7,39 +16,50 @@ def read_file(path):
     return data
 
 
-def check_policy(string: str):
+def decompose_string(string: str) -> Password:
+    """
+    No teksta izgriež visus nepieciešamos laukus un agtriež tos kā Password klasi.
+    Piemērs:
+      '1-3 a: abcde' -> Password(number_a=1, number_b=3, character='a', password='abcde')
+    """
     pattern = r"^(\d+)-(\d+)\s+(.):\s(.+)$"
+    number_a, number_b, character, password = re.match(pattern, string).groups()
 
-    lower_limit, upper_limit, character, password = re.match(pattern, string).groups()
+    return Password(
+        number_a=int(number_a),
+        number_b=int(number_b),
+        character=character,
+        password=password,
+    )
 
-    character_count = password.count(character)
 
-    if character_count >= int(lower_limit) and character_count <= int(upper_limit):
+def check_policy_count(string: str) -> bool:
+    p = decompose_string(string=string)
+    character_count = p.password.count(p.character)
+    if character_count >= p.number_a and character_count <= p.number_b:
         return True
     else:
         return False
 
 
 def check_policy_position(string: str):
-    pattern = r"^(\d+)-(\d+)\s+(.):\s(.+)$"
+    p = decompose_string(string=string)
 
-    position_a, position_b, character, password = re.match(pattern, string).groups()
-
-    if (password[int(position_a) - 1] == character) + (
-        password[int(position_b) - 1] == character
+    if (p.password[p.number_a - 1] == p.character) + (
+        p.password[p.number_b - 1] == p.character
     ) == 1:
         return True
     else:
         return False
 
 
-inputs = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
-assert sum([check_policy(i) for i in inputs]) == 2
+inputs_test = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+assert sum([check_policy_count(i) for i in inputs_test]) == 2
 
-data = read_file("data/day_02.txt")
-good_passwords = sum([check_policy(i) for i in data])
-print(good_passwords)
+inputs_real = read_file("data/day_02.txt")
+good_passwords = sum([check_policy_count(i) for i in inputs_real])
+print(f"Atbilde pirmajam jautājumam: {good_passwords}")
 
-assert sum([check_policy_position(i) for i in inputs]) == 1
-good_passwords = sum([check_policy_position(i) for i in data])
-print(good_passwords)
+assert sum([check_policy_position(i) for i in inputs_test]) == 1
+good_passwords = sum([check_policy_position(i) for i in inputs_real])
+print(f"Atbilde otrajam jautājumam: {good_passwords}")
