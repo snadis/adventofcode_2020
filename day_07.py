921d2d84216b7eb78ac8d7b7e0844d923c25239f
snadis <snadis@upp.lv>
2020-12-10 21:45:54 +0200

    pakomentēts

--- day_07.py
+++ day_07.py
@@ -29,29 +29,42 @@ class Bag:
 
 def read_data(path: str) -> str:
     with open(file=path, mode="r") as f:
-        return f.read()
+        return f.read().strip()
 
 
 def parse_line(line: str) -> Bag:
-
+    # sadala rindu divās daļās pēc teksta
     bag, subbags = line.split(" bags contain ")
 
+    # dict ar apakšomām
     contains = {}
+    # pēc komdta sadala bērnsomas
     for b in subbags.split(","):
+        # ja bērnsomu nav, tad iet pie nēkamās somas
         if b.strip() == "no other bags.":
             continue
+        # regex atrgriež grupas ar skaitu un krāsu
         regex = r"(\d+)\s(.*)\sbag"
         count, color = re.match(regex, b.strip()).groups()
+        # pievieno bērnsomu vārdnīcai
         contains[color] = count
 
     return Bag(color=bag, children=contains)
 
 
 def make_bags(input: str) -> list[Bag]:
+    """Nolasa visas rindiņas un noparsē"""
     return [parse_line(line) for line in input.split("\n")]
 
 
-def make_parent_dict(bags: list[Bag]):
+def make_parent_dict(bags: list[Bag]) -> dict[str, list[str]]:
+    """
+    Izveido vērtnīcu katrai somai ar sarakstu kurā ir visas sīs somas iespējamās
+    vecāka somas.
+
+    Piemēram: {'muted yellow': ['light red', 'dark orange']}
+    muted yellow var atrasties pa tiešo light red un dark orange somās
+    """
     contains = defaultdict(list)
     for bag in bags:
         for subbag in bag.children:
@@ -65,11 +78,16 @@ def outermost_bag_colors(bags: list[Bag], color: str) -> list[str]:
 
     valid_colors = set()
 
+    # ieliek rindā meklējamo krāsu
     stack = [color]
     while stack:
+        # nolasa no rindas krāsu
         check = stack.pop()
+        # atgrieš krāsas iespējamos vecākus vai tukšu sarkastu
         for child in parents.get(check, []):
+            # iespējamām krāsām pievieno visas vecāksomas
             valid_colors.add(child)
+            # vecāk somas pievienot rindai, lai pārbaudīt vai tām nav vēl citas vecāksomas
             stack.append(child)
 
     return list(valid_colors)
@@ -81,15 +99,22 @@ def number_of_bags_inside_bag(bags: list[Bag], color: str) -> int:
 
     number_of_bags = 0
 
+    # pievieno rindā pirmo vērību, krāsu un 1, jo 1 soma
     stack = [(color, 1)]
     while stack:
         color, count = stack.pop()
 
+        # pārskata somas bērnu somas
         for child_color, child_count in ludge[color].items():
 
+            # skaits ir somu skaits reizināts ar vecāku skaitu
+            # piemēram, ja 1 -> 2 -> 2 -> 5 tad pirmajā līmenī ir 1 soma, otrajā
+            # 1 * 2 = 2, un trešajā 1 * 2 * 2 = 4....
+            # 1 -> 2 -> 4 -> 20
             child_count = int(child_count) * count
+            # pieskaita somas
             number_of_bags = number_of_bags + child_count
-
+            # pieiveno rindā tālākai pārbaudei bērnsomas ar uzlabotajiem skaitiem
             stack.append((child_color, child_count))
 
     return number_of_bags
